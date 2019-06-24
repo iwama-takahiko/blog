4d6e23da6fcd61930a1c2c8739f8c39862870b33
iwama-takahiko <ae13013i@gmail.com>
2019-06-24 17:42:43 +0900

    user index add

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
-  } 
+    registrations:  'users/registrations',
+    sessions:  'users/sessions'
+  }
+  resources :users, only:[:index,:show]
 
-  devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy" 
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "posts#index"
   resources :posts do
   end
-  resources :users, only:[:show]
 end
